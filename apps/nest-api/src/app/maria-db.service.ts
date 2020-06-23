2fdabfe251dadb2cb6985b66075c10704fc79129
robport <26696668+robport@users.noreply.github.com>
2020-06-23 15:45:59 +0100

    Added DB reconnect logic on error event

--- apps/nest-api/src/app/maria-db.service.ts
+++ apps/nest-api/src/app/maria-db.service.ts
@@ -4,13 +4,20 @@ import { Connection, createConnection } from 'mariadb';
 @Injectable()
 export class MariaDbService {
   conn: Connection;
+  logger = new Logger('MariaDbService');
 
   async getConnection(): Promise<Connection> {
     if (!this.conn || !this.conn.isValid()) {
       const defaultHostUrl = 'mariadb://system:system@localhost/personal';
       const hostUrl = process.env.MARIA_DB_URL || defaultHostUrl;
-      Logger.log(`Create Connection to Maria DB ${hostUrl}`);
+      this.logger.log(`Create Connection to Maria DB`);
       this.conn = await createConnection(hostUrl);
+      this.logger.log(`Connection created`);
+
+      const x = this.conn.on('error', async err => {
+        this.logger.error(err, 'Error event received');
+        this.conn = null;
+      });
     }
     return this.conn;
   }
